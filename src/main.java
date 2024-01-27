257ee0a3b9b92f62db13f157312367e425f31207
Shmidt TV <tan_8@mail.ru>
2024-01-27 22:35:20 +0700

    Gitignore

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        int price = 10_000;
+
+        int mile1 = price / 20;
+
+
+        System.out.println("Бонус: " + mile1);
+    }
+}
